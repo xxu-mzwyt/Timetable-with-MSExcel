54a1c63edc54c37813c379b1927566bd68841dae
Benjamin Hsu <mr_miser@163.com>
2020-09-15 11:53:41 +0800

    Update display format

--- display.py
+++ display.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 
 root = tk.Tk()
 
-root.wm_attributes("-alpha", 0.6)
+root.wm_attributes("-alpha", 0.71)
 # root.wm_attributes("-toolwindow", True)
 # root.wm_attributes("-topmost", True)
 root.overrideredirect(True)
@@ -23,8 +23,10 @@ img = img.resize((450, 150), Image.ANTIALIAS)
 
 
 photo = ImageTk.PhotoImage(img)
-label_img = tk.Label(root, image = photo)
-label_img.pack()
+lframe = tk.Frame(root, bg="#272727")
+lframe.pack()
+label_img = tk.Label(lframe, image = photo, bg='#181818', bd = 10,)
+label_img.pack(padx=1, pady=1)
 
-root.geometry("+780+530")
+root.geometry("+770+535")
 root.mainloop()
