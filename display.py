cefd3f3ce0ff058185093a8b56343f8b4a64bdb6
Benjamin Hsu <mr_miser@163.com>
2020-09-16 21:16:07 +0800

    Update display.py

--- display.py
+++ display.py
@@ -25,8 +25,27 @@ img = img.resize((450, 150), Image.ANTIALIAS)
 photo = ImageTk.PhotoImage(img)
 lframe = tk.Frame(root, bg="#272727")
 lframe.pack()
-label_img = tk.Label(lframe, image = photo, bg='#181818', bd = 10,)
+label_img = tk.Label(lframe, image = photo, bg='#181818', bd = 10)
 label_img.pack(padx=1, pady=1)
+
+cnt = 0
+
+def motion(event):
+    global cnt
+    print("event", cnt, event)
+    cnt += 1
+    root.wm_attributes("-topmost", True)
+    root.wm_attributes("-topmost", False)
+    # root.state('iconic')
+    #        WIN32                  VK_LWIN + D
+
+# root.bind('<FocusOut>', motion)
+
+back_window = tk.Toplevel()
+back_window.wm_attributes("-topmost", True)
+back_window.overrideredirect(True)
+
+back_window.bind('<Button-1>', motion)
 
 root.geometry("+770+535")
 root.mainloop()
