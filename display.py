50ffdf6716113cf3518b6840123b3db754eeeaf1
Benjamin Hsu <mr_miser@163.com>
2020-09-17 07:42:21 +0800

    Update display.py
    Still not fixed

--- display.py
+++ display.py
@@ -1,26 +1,70 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import re
+import win32gui, win32con, win32com.client, win32api
+import time
 
 
-root = tk.Tk()
+def _window_enum_callback(hwnd, wildcard):
+
+    if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
+        win32gui.BringWindowToTop(hwnd)
+        # 先发送一个alt事件，否则会报错导致后面的设置无效：pywintypes.error: (0, 'SetForegroundWindow', 'No error message is available')
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.SendKeys('%')
+        # 设置为当前活动窗口
+        win32gui.SetForegroundWindow(hwnd)
+
+def wake(event):
+    # WIN32 WIN + D
+    win32api.keybd_event(91, 0, 0, 0)
+    win32api.keybd_event(68, 0, 0, 0)
+    win32api.keybd_event(68, 0, win32con.KEYEVENTF_KEYUP, 0)
+    win32api.keybd_event(91, 0, win32con.KEYEVENTF_KEYUP, 0)
+
+    # Wake timetable window
+    # root.wm_attributes("-topmost", True)
+    # root.wm_attributes("-topmost", False)
+
+    time.sleep(0.1)
+
+    pop = tk.Toplevel()
+    pop.destroy()
+
+    time.sleep(1)
+
+    try:
+        back_backup.destroy()
+    except:
+        pass
+
+    back_backup = tk.Toplevel()
+    back_backup.wm_attributes("-topmost", True)
+    back_backup.overrideredirect(True)
+    back_backup.geometry("10x50+1362+728")
+
+    back_backup.bind('<Button-1>', wake)
+
+    # win32gui.EnumWindows(_window_enum_callback, ".*%s.*" % 'tk')
+    # win32gui.EnumWindows(_window_enum_callback, ".*%s.*" % 'maintk')
+
+back_window = tk.Tk()#bg='green')#"#1d1d1d")
+# back_window.wm_attributes("-alpha", 0.01)
+back_window.wm_attributes("-topmost", True)
+back_window.overrideredirect(True)
+back_window.geometry("10x50+1362+728")
+# back_window.geometry("100x100+0+720")
+
+back_window.bind('<Button-1>', wake)
+
+
+root = tk.Toplevel()
 
 root.wm_attributes("-alpha", 0.71)
-# root.wm_attributes("-toolwindow", True)
-# root.wm_attributes("-topmost", True)
 root.overrideredirect(True)
 
-
 img = Image.open('img.png')
-
-# print(img.size())
-# w = img.width()
-# h = img.height()
-
-# set_height = 200
-# w = w * set_height / h
-# h = set_height
 img = img.resize((450, 150), Image.ANTIALIAS)
-
 
 photo = ImageTk.PhotoImage(img)
 lframe = tk.Frame(root, bg="#272727")
@@ -28,24 +72,6 @@ lframe.pack()
 label_img = tk.Label(lframe, image = photo, bg='#181818', bd = 10)
 label_img.pack(padx=1, pady=1)
 
-cnt = 0
-
-def motion(event):
-    global cnt
-    print("event", cnt, event)
-    cnt += 1
-    root.wm_attributes("-topmost", True)
-    root.wm_attributes("-topmost", False)
-    # root.state('iconic')
-    #        WIN32                  VK_LWIN + D
-
-# root.bind('<FocusOut>', motion)
-
-back_window = tk.Toplevel()
-back_window.wm_attributes("-topmost", True)
-back_window.overrideredirect(True)
-
-back_window.bind('<Button-1>', motion)
-
 root.geometry("+770+535")
-root.mainloop()
+
+back_window.mainloop()
